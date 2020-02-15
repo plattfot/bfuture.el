3567219a2a0b433585552dddb95724ce8469f28e
Fredrik Salomonsson <plattfot@gmail.com>
2020-02-15 13:59:55 -0800

    Updated documentation

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # bfuture.el
 Basic future concept for emacs with tramp support
+
+Inspired by [https://github.com/Alexander-Miller/pfuture](pfuture)
+
+Have the basic concepts of spawning a process and retrieving the
+output. This works with tramp to launch processes on a remote host.
+Which is sadly not working in pfuture. But looses the ability to tell
+the stdout and stderr apart.
+
+
+
--- bfuture.el
+++ bfuture.el
@@ -1,11 +1,12 @@
 ;;; bfuture --- basic future concept -*- lexical-binding: t -*-
 
 ;;; Commentary:
-;; Based on the code of pfuture.el - https://github.com/Alexander-Miller/pfuture
+;; Inspired by pfuture.el - https://github.com/Alexander-Miller/pfuture
 
 ;; Have the basic concepts of spawning a process and retrieving the
-;; output. But this works with tramp to launch processes on a remote
-;; host. Which is sadly not working in pfuture.
+;; output. This works with tramp to launch processes on a remote host.
+;; Which is sadly not working in pfuture. But looses the ability to
+;; tell the stdout and stderr apart.
 
 ;;; Code:
 (require 'cl-lib)
